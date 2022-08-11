942a3d115498b60542f751098ff392560ed85c37
IsaiasGustavo <isaiasipbi56@gmail.com>
2022-08-11 15:08:54 -0300

    meu primeiro commit

---
+++ app.js
@@ -0,0 +1,11 @@
+/*Criar botão*/
+
+var btpj = document.getElementById("btpj");
+
+btpj.addEventListener("click", function() {
+
+    var pj = document.getElementById("pj");
+    pj.classList.toggle("show");
+});
+
+
